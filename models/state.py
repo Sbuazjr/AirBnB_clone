1a992fe12f7d262cf7a96f54ed83e19b58133c67
sbuazjr <sbuazjr1115@gmail.com>
2022-10-29 05:53:53 +0000

    console

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that define the state class"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """class that represents state
+    Attribute:
+	    name(str): name of the state
+    """
+    name = ""
